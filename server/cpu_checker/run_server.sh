abaccb6b8e2c07d0f0648afc8a07d0f1df55c293
Ivan <demon_cuc@mail.ru>
2024-05-07 03:59:20 +0400

    README.md, скрипт для запуска сервера

--- server/cpu_checker/run_server.sh
+++ server/cpu_checker/run_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-SERVER_IP=${1:"127.0.0.1"}
-SERVER_PORT=${2:8001}
+SERVER_IP = ${1:-127.0.0.1}
+SERVER_PORT = ${2:-8001}
 
-
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 python3 ./manage.py runserver SERVER_IP:SERVER_PORT
-echo "server is running"
+echo "server is running at $SERVER_IP:$SERVER_PORT"
