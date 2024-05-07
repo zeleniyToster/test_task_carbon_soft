c3180a8be0a86ce886a3c0bdd7880e00ac18525f
Ivan <demon_cuc@mail.ru>
2024-05-07 06:08:49 +0400

    поправил скрипт

--- client/cpu_utilization_script.sh
+++ client/cpu_utilization_script.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+DEFAULT_IP=127.0.0.1
+DEFAULT_PORT=8001
 # IP и порт сервера, куда отправлять данные
-#SERVER_IP="127.0.0.1"
-#SERVER_PORT="$1"
+SERVER_IP="${1:-$DEFAULT_IP}"
+SERVER_PORT="${2:-$DEFAULT_PORT}"
 
 while true; do
     # Получить текущую утилизацию CPU (%)
     CPU_UTILIZATION=$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
-    echo "$(date)" "CPU_UTILIZATION" $CPU_UTILIZATION"%" >> ./cpu_utilization_sript_log.log
 
     # Отправить данные на сервер
-    #curl -X POST -d "cpu_utilization=$CPU_UTILIZATION" "http://$SERVER_IP:$SERVER_PORT/"
+    curl -X POST -d "cpu_utilization=$CPU_UTILIZATION" "http://$SERVER_IP:$SERVER_PORT/cpu/"
 
     # Подождать 10 секунд перед следующей отправкой
     sleep 10
