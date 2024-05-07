518b07bc1a1169cb370ef9eecf66358eca64db45
Ivan <demon_cuc@mail.ru>
2024-05-07 06:54:16 +0400

    docstrings

--- server/cpu_checker/cpu_checker_service/views.py
+++ server/cpu_checker/cpu_checker_service/views.py
@@ -7,6 +7,7 @@ from cpu_checker_service.orm import add_cpu_util_data, get_hundred_last_records
 
 
 def index(request: WSGIRequest) -> HttpResponse:
+    """ Главная страница"""
     cpu_utilization_data = get_hundred_last_records()
     context = {
         'cpu_datas': cpu_utilization_data
@@ -16,11 +17,15 @@ def index(request: WSGIRequest) -> HttpResponse:
 
 @csrf_exempt
 def cpu_utilization_data_handler(request: WSGIRequest):
-    cpu_util_data = request.POST
-    add_cpu_util_data(cpu_util_data)
+    """ Энд-поинт для получения данных с клиента"""
+    if request.method == "POST":
+        cpu_util_data = request.POST
+        add_cpu_util_data(cpu_util_data)
+
     return HttpResponse()
 
 
 def ajax_cpu_endpoint(request):
+    """ Энд-поинт возвращающий последние 100 сериализованных записей."""
     cpu_utilization_data = get_hundred_last_records()
     return HttpResponse(cpu_utilization_data, content_type="application/json")
